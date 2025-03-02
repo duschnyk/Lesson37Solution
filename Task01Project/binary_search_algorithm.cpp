7351db4251d318b7c779e99c5a2734db9393dfea
giganig <matveykaschuk@gmail.com>
2025-03-02 18:18:48 +0300

    binary_find_value update

--- Task01Project/binary_search_algorithm.cpp
+++ Task01Project/binary_search_algorithm.cpp
@@ -1,6 +1,24 @@
 #include "binary_search_algorithm.h"
 
 bool binary_find_value(int* array, int size, int value) {
+	int first = 0;
+	int last = size - 1;
+	//!
+	while (first <= last) {
+		int mid = (first + last) / 2;
+
+		if (value == array[mid]) {
+			return true;
+		}
+		else
+			if (value < array[mid]) {
+				last = mid - 1;
+			}
+			else {
+				first = mid + 1;
+			}
+	}
+
 	for (int i = 0; i < size; i++)
 	{
 
